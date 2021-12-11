8d6744959c16d42bc8084371f5897fa598fdc93a
Muhammad Danil Muis <daniltelkom@gmail.com>
2021-12-11 19:18:40 +0700

    feature done

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ import pickle
 
 app = Flask(__name__)
 
-# model_file = open('model.pkl', 'rb')
-model_file = open('model_real.pkl', 'rb')
+model_file = open('model.pkl', 'rb')
 model = pickle.load(model_file, encoding='bytes')
 
 @app.route('/')
@@ -17,25 +16,34 @@ def predict():
     Predict the insurance cost based on user inputs
     and render the result to the html page
     '''
-    age, sex, smoker = [x for x in request.form.values()]
+    a,b,c,d,e = [x for x in request.form.values()]
 
-    data = []
+    # data = []
 
-    data.append(int(age))
-    if sex == 'Laki-laki':
-        data.extend([0, 1])
-    else:
-        data.extend([1, 0])
+    # data.append(int(age))
+    # if sex == 'Laki-laki':
+    #     data.extend([0, 1])
+    # else:
+    #     data.extend([1, 0])
 
-    if smoker == 'Ya':
-        data.extend([0, 1])
-    else:
-        data.extend([1, 0])
-    print(data)
+    # if smoker == 'Ya':
+    #     data.extend([0, 1])
+    # else:
+    #     data.extend([1, 0])
+    data = [e,d,c,b,a]
     prediction = model.predict([data])
-    output = round(prediction[0], 2)
-
-    return render_template('index.html', insurance_cost=output, age=age, sex=sex, smoker=smoker)
+    output1 = round(prediction[0])
+    
+    data2 = [d,c,b,a,output1]
+    prediction = model.predict([data2])
+    output2 = round(prediction[0])
+    
+    
+    data3 = [c,b,a,output1,output2]
+    prediction = model.predict([data3])
+    output3 = round(prediction[0])
+    
+    return render_template('index.html', output1=output1, output2=output2, output3=output3)
 
 
 if __name__ == '__main__':
